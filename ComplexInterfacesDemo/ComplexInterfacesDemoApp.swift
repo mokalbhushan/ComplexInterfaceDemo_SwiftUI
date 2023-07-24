07bbf2a74fb5e6851e88f2f974fe7989ad219b6c
Bhushan Mokal <bhushan.mokal@neosoftmail.com>
2023-07-24 16:56:22 +0530

    Initial Commit

---
+++ ComplexInterfacesDemo/ComplexInterfacesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexInterfacesDemoApp.swift
+//  ComplexInterfacesDemo
+//
+//  Created by Bhushan Mokal on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ComplexInterfacesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
